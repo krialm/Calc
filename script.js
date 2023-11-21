3b92b0f0a96fa05209a98ee7c0d310b36cd2d3dd
krialm <konotopa20005@gmail.com>
2023-11-21 20:19:02 +0200

    apple redesign

---
+++ script.js
@@ -0,0 +1,83 @@
+let currentInput = '0';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    if (currentInput === '0' || currentInput === '-0') {
+        currentInput = number.toString();
+    } else {
+        currentInput += number;
+    }
+    updateDisplay();
+}
+
+function appendDecimal() {
+    if (!currentInput.includes('.')) {
+        currentInput += '.';
+        updateDisplay();
+    }
+}
+
+function cleanInput() {
+    currentInput = '0';
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function changeSign() {
+    currentInput = (parseFloat(currentInput) * -1).toString();
+    updateDisplay();
+}
+
+function percentage() {
+    currentInput = (parseFloat(currentInput) / 100).toString();
+    updateDisplay();
+}
+
+function operation(selectedOperator) {
+    if (operator !== '' && previousInput !== '') {
+        calculate();
+    }
+    operator = selectedOperator;
+    previousInput = currentInput;
+    currentInput = '0';
+}
+
+function calculate() {
+    let result;
+    const num1 = parseFloat(previousInput);
+    const num2 = parseFloat(currentInput);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                alert('Cannot divide by zero');
+                cleanInput();
+                return;
+            }
+            break;
+        default:
+            break;
+    }
+
+    currentInput = result.toString();
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('input').value = currentInput;
+}
